a3e42ebd18a81fcbfdacb645a1e295ecaf0ad361
jay <jayaram.hariharan@utexas.edu>
2020-09-15 13:58:31 -0400

    add unit tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,3 +24,70 @@ class TestNoStratigraphyError:
         _mtch = "'str' object has no attribute 'somevar'."
         with pytest.raises(utils.NoStratigraphyError, match=_mtch):
             raise utils.NoStratigraphyError('someobj', 'somevar')
+
+
+def test_format_number_float():
+    _val = float(5.2)
+    _fnum = utils.format_number(_val)
+    assert _fnum == '10'
+
+    _val = float(50.2)
+    _fnum = utils.format_number(_val)
+    assert _fnum == '50'
+
+    _val = float(15.0)
+    _fnum = utils.format_number(_val)
+    assert _fnum == '20'
+
+
+def test_format_number_int():
+    _val = int(5)
+    _fnum = utils.format_number(_val)
+    assert _fnum == '0'
+
+    _val = int(6)
+    _fnum = utils.format_number(_val)
+    assert _fnum == '10'
+
+    _val = int(52)
+    _fnum = utils.format_number(_val)
+    assert _fnum == '50'
+
+    _val = int(15)
+    _fnum = utils.format_number(_val)
+    assert _fnum == '20'
+
+
+def test_format_table_float():
+    _val = float(5.2)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '5.2'
+
+    _val = float(50.2)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '50.2'
+
+    _val = float(15.0)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '15.0'
+
+    _val = float(15.03689)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '15.0'
+
+    _val = float(15.0689)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '15.1'
+
+def test_format_table_float():
+    _val = int(5)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '5'
+
+    _val = int(5.8)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '5'
+
+    _val = int(5.2)
+    _fnum = utils.format_table(_val)
+    assert _fnum == '5'
